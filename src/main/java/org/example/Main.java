5e5af7b5110ff369c42199c6d66d7e48ab421a8a
Марк Міллер <youtte1g@gmail.com>
2022-11-12 11:43:32 +0200

    final commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String finalResult = toBinaryString(6);
+        System.out.println(finalResult);
+    }
+
+    public static String toBinaryString(int value) {
+        if (value == 0) {
+            return String.valueOf(0);
+        }
+        StringBuilder toBinary = new StringBuilder();
+        int a;
+        while (value != 0) {
+            a = value % 2;
+            toBinary.append(a);
+            value = value / 2;
+        }
+        return String.valueOf(toBinary.reverse());
     }
 }
